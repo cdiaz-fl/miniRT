737f54a8bd6e959967f8890c1d7e0e279b69fbce
zcanales <zaloacanales@gmail.com>
2022-04-20 11:11:42 +0200

    minicambio

--- vector.h
+++ vector.h
@@ -6,7 +6,7 @@
 /*   By: zcanales <zcanales@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 09:39:40 by zcanales          #+#    #+#             */
-/*   Updated: 2022/04/20 11:04:27 by zcanales         ###   ########.fr       */
+/*   Updated: 2022/04/20 11:11:20 by zcanales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@
 # include <stdlib.h>
 
 /*PRINT TUPLE*/
-void print_tuple(float *tpl); 	//BORRAR
+void	print_tuple(float *tpl); 	//BORRAR
 
 /*CREAR UN PUNTO O VECTOR*/
-float *create_tuple(float x, float y, float z, float w);
+float	*create_tuple(float x, float y, float z, float w);
 
 /*OPERACIONES ENTRE VECTORES Y PUNTOS*/
-float *add_tuple(float *tpl1, float* tpl2);
-float *sub_tuple(float *tpl1, float* tpl2);
+float	*add_tuple(float *tpl1, float* tpl2);
+float	*sub_tuple(float *tpl1, float* tpl2);
 
 /*OPERACIONES ENTRE VECTORES*/
 //vector_operation1
-float *scalar_mul_vec(float *tpl, float scalar);
-float *scalar_div_vec(float *tpl, float scalar);
-float dot_product_vec(float *tpl1, float *tpl2);
-float *cross_product_vec(float *a, float *b);
+float	*scalar_mul_vec(float *tpl, float scalar);
+float	*scalar_div_vec(float *tpl, float scalar);
+float	dot_product_vec(float *tpl1, float *tpl2);
+float	*cross_product_vec(float *a, float *b);
 //vector_operation2
-float *normalization_vec(float *tpl, float magnitude);
-float magnitude_vec(float *tpl);
-float *neg_vec(float *tpl);
+float	*normalization_vec(float *tpl, float magnitude);
+float	magnitude_vec(float *tpl);
+float	*neg_vec(float *tpl);
 
 #endif
